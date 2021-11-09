fe32140a05a0bd9f624e6d4cfaa2f0069255781e
robin828 <61390860+robin828@users.noreply.github.com>
2021-11-10 04:53:41 +0530

    working

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,55 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import React from 'react'
-import { Home, NotFound } from './Components/default';
-import Header from './Components/Header/Header';
-import DetailView from './Components/ItemDetails/DetailView';
-import TemplateProvider from './templates/TemplateProvider';
-import ContextProvider from './context/ContextProvider';
-import Cart from './Components/Cart/Cart';
-import Products from './Components/Home/Products';
-import ItemDetail  from './Components/ItemDetails/ItemDetail';
-import UploadProduct  from './Components/ItemDetails/UploadProduct';
-import { Box } from '@material-ui/core'
-import CartView from './Components/Cart/CartView'
-import Checkout from './Components/Cart/Checkout';
-import OrderFailure from './Components/Cart/OrderFailure';
-import Admin from './Components/Admin/Admin';
-import OrderSuccess from './Components/Cart/OrderSuccess';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+import { Home, NotFound } from "./Components/default";
+import Header from "./Components/Header/Header";
+import DetailView from "./Components/ItemDetails/DetailView";
+import TemplateProvider from "./templates/TemplateProvider";
+import ContextProvider from "./context/ContextProvider";
+import Cart from "./Components/Cart/Cart";
+import Products from "./Components/Home/Products";
+import ItemDetail from "./Components/ItemDetails/ItemDetail";
+import AllUser from "./Components/ItemDetails/AllUser";
+import AllProduct from "./Components/ItemDetails/AllProduct";
+import AllPdf from "./Components/ItemDetails/AllPdf";
+import AddProduct from "./Components/ItemDetails/AddProduct";
+import UploadProduct from "./Components/ItemDetails/UploadProduct";
+import { Box } from "@material-ui/core";
+import CartView from "./Components/Cart/CartView";
+import Checkout from "./Components/Cart/Checkout";
+import OrderFailure from "./Components/Cart/OrderFailure";
+import Admin from "./Components/Admin/Admin";
+import OrderSuccess from "./Components/Cart/OrderSuccess";
 
 function App() {
-  const [ text, setText ] = React.useState();
+  const [text, setText] = React.useState();
 
   return (
+    // <div></div>
     <TemplateProvider>
       <ContextProvider>
         <BrowserRouter>
           <Header text={text} setText={setText} />
-          <Box style={{marginTop: 54}}>
+          <Box style={{ marginTop: 54 }}>
             <Switch>
-              <Route exact path= '/' component={Home} />
-              <Route exact path= '/cart' component={Cart} />
+              <Route exact path="/" component={Home} />
+              <Route exact path="/cart" component={Cart} />
               {/* <Route exact path= '/product' render={()=<Products  />} /> */}
-              <Route path='/products' component={()=>(
-                       <Products text={text} setText={setText} />
-                       )}/>
-              <Route exact path= '/upload/product' component={UploadProduct} />
-              <Route exact path= '/product/:id' component={ItemDetail} />
-              <Route exact path= '/checkout' component={Checkout} />
-              <Route exact path= '/orderfailure' component={OrderFailure} />
-              <Route exact path= '/ordersuccess' component={OrderSuccess} />
-              <Route exact path= '/admin' component={Admin} />
+              <Route
+                path="/products"
+                component={() => <Products text={text} setText={setText} />}
+              />
+              <Route exact path="/upload/product" component={UploadProduct} />
+              <Route exact path="/product/form" component={AddProduct} />
+              <Route exact path="/product/:id" component={ItemDetail} />
+              <Route exact path="/checkout" component={Checkout} />
+              <Route exact path="/orderfailure" component={OrderFailure} />
+              <Route exact path="/ordersuccess" component={OrderSuccess} />
+              <Route exact path="/admin/user/all" component={AllUser} />
+              <Route exact path="/admin/user/product" component={AllProduct} />
+              <Route exact path="/admin/user/pdf" component={AllPdf} />
+              <Route exact path="/admin" component={Admin} />
+              {/* <Route exact path= '/admin' component={Admin} /> */}
 
-              
-              
               {/* <UploadProduct /> */}
               <Route component={NotFound} />
             </Switch>
--- client/src/Components/ItemDetails/UploadProduct.jsx
+++ client/src/Components/ItemDetails/UploadProduct.jsx
@@ -1,7 +1,20 @@
 import React, {useState} from 'react';
+import { Typography, Grid, Paper, TextField, Button, TableCell, TableRow, TableBody, TableHead, Box, Table, TablePagination } from '@material-ui/core';
+import axios from 'axios';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { Link  } from 'react-router-dom';
+
 
 const UploadProduct = () => {
     const [selectedFile, setSelectedFile] = useState();
+    const [mainData, setMainData] = useState([]);
+    const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+    React.useEffect(()=>{
+        const userId = localStorage.getItem("userId");
+
+        axios.get(`http://localhost:7000/pdf/${userId}`).then(res=>{console.log(res.data);setMainData(res.data)})
+    }, [])
 	// const [isFilePicked, setIsFilePicked] = useState(false);
 
     const changeHandler = (event) => {
@@ -9,13 +22,102 @@ const UploadProduct = () => {
 		// setIsSelected(true);.
         console.log(event.target.files[0]);
 	};
+    const handleSubmit = () => {
+        const formData = new FormData();
+        const userId = localStorage.getItem("userId");
+        formData.append("file", selectedFile);
+        // axios.post(`http://localhost:7000/pdf/submitpdf/${userId}`, formData).then(res=>console.log(res.data))
+    }
+    const handleLimitChange = (event) => {
+        setLimit(event.target.value);
+      };
+    
+      const handlePageChange = (event, newPage) => {
+        setPage(newPage);
+      };
+    
     return (
-        <div>
-            <input type="file" name="file" onChange={changeHandler} />
+        <>
+        <div style={{textAlign:'center', marginTop:'150px'}} >
+            <Typography>Add Your Pdf Here</Typography>
+            <input style={{marginTop:'55px'}} type="file" name="file" onChange={changeHandler} />
+            <Button onClick={handleSubmit} >Submit</Button>
 			<div>
+            <Typography style={{marginTop:'55px'}}>Your recent pdf</Typography>
+            
 				{/* <button onClick={handleSubmission}>Submit</button> */}
 			</div>
-        </div>
+
+            
+       
+
+<PerfectScrollbar>
+        <Box minWidth={1050}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  User
+                </TableCell>
+                <TableCell style={{textAlign:'center'}}>
+                  Status
+                </TableCell>
+                <TableCell>
+                  Action
+                </TableCell>
+              </TableRow>
+            </TableHead>
+             <TableBody>
+              {mainData?mainData.slice((page)*limit,(page)*limit+limit).map((proc) => (
+                   (
+                <TableRow
+                  hover
+                  key={proc.id}
+                //   selected={selectedCustomerIds.indexOf(proc.id) !== -1}
+                >
+                  <TableCell>
+                    {/* {proc.user_id?proc.user_id.username:''} */}
+                  </TableCell>
+                  
+                  <TableCell>
+                    {/* {getStatus(proc.status)} */}
+                    {proc.isApproved}hi
+                  </TableCell>
+                  {/* <TableCell>
+                    {getShipmentStatus(proc.shipment ? proc.shipment.shipped_on ?proc.shipment.shipped_on :'NA':'NA')}
+                  </TableCell> */}
+                  <TableCell>
+                      {proc.isApproved ? <Link to="/product/form" >
+                  <Button variant="contained" color="primary" >
+                          View
+                  </Button>
+                  </Link> : <Link>
+                  <Button variant="contained" color="primary" >
+                          Rejected
+                  </Button>
+                  </Link>}
+                      
+                  </TableCell>
+                </TableRow>
+                  )
+              )):''}
+              
+            </TableBody>
+          </Table>
+        </Box>
+      </PerfectScrollbar>
+      <TablePagination
+        component="div"
+        count={mainData?mainData.length:''}
+        onChangePage={handlePageChange}
+        onChangeRowsPerPage={handleLimitChange}
+        page={page}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    
+    </div>
+        </>
     )
 }
 
